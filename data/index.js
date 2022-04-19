cb549cd7c5715d481dcaf63b3826dd95f3daf48f
alejandro945 <alejo8677@gmail.com>
2022-04-18 19:47:59 -0500

    :fire: Bussiness Logic

--- data/index.js
+++ data/index.js
@@ -1,11 +1,38 @@
-"question" = {
-    "id":0,
-    "title": "",
-    "answers":[
-        "answer"={
-            "id":0,
-            "state":false,
-            "text":""
+data = [
+    "teachers" = [
+        "teacher" = {
+            "userName": "",
+            "password": "",
+            "forms": [
+                "form" = {
+                    "code": 0,
+                    "avaible": true,
+                    "questions": [
+                        "question" = {
+                            "id": 0,
+                            "title": "",
+                            "answers": [
+                                "answer" = {
+                                    "id": 0,
+                                    "state": false,
+                                    "text": ""
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ],
+    "students" = [
+        "student" = {
+            "username": "",
+            "password": ""
         }
     ]
-}
+
+]
+
+
+
+
